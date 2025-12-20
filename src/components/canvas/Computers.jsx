e720fc3768c96be7937f99d17a375cab6405f5cc
Rajarshi Das <144707206+noobremon@users.noreply.github.com>
2025-12-20 20:44:37 +0530

    modified:   src/components/canvas/Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -65,6 +65,7 @@ const ComputersCanvas = () => {
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
+          enableRotate={!isMobile}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
