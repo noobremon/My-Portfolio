73189a4454e4f4c6d7c7ce243bea1b8c696a9ee4
Rajarshi Das <144707206+noobremon@users.noreply.github.com>
2026-07-30 18:37:29 +0530

    new file:   src/assets/eventflow.png 	modified:   src/assets/index.js 	new file:   src/assets/medicine_project.png 	modified:   src/constants/index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -11,7 +11,7 @@ import framer from './tech/framer.png';
 import css from "./tech/css.png";
 import figma from "./tech/figma.png";
 import gearXpert from './gearXpert.png';
-import project3 from './project3.webp';
+import project3 from './medicine_project.png';
 import project2 from './proj1.webp'
 import mysql from './tech/mysql.png';
 import express from './tech/express.png';
@@ -37,7 +37,9 @@ import pcbProject from './pcb_project.png';
 import railwayProject from './railway_project.png';
 import toolboxProject from './toolbox_project.png';
 import linkClick from './link_click.png';
-import carrent from './carrent.png';
+import carrent from './eventflow.png';
+import eventflow from './eventflow.png';
+import medicineProject from './medicine_project.png';
 
 export {
   logo,
@@ -53,6 +55,8 @@ export {
   project2,
   project3,
   carrent,
+  eventflow,
+  medicineProject,
   pcbProject,
   railwayProject,
   toolboxProject,
