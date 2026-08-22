464021b25030e3105aaa9a809bd9a8b057e487f5
Rajarshi Das <144707206+noobremon@users.noreply.github.com>
2026-08-22 22:24:59 +0530

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Resume, Tech, Works, StarsCanvas } from "./components";
+import { About, Contact, Experience, Hero, Navbar, Resume, Tech, Works, StarsCanvas } from "./components";
 import Footer from "./components/Footer";
 
 const App = () => {
@@ -14,7 +14,6 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
         <Resume />
         <Contact />
         <Footer />
